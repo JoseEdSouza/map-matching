fe8135500bc6ce196a899ede35665239604556bb
José Souza <joseedsouza.dev@gmail.com>
2025-06-18 01:37:12 -0300

    Update osmosis.sh to create PBF output from XML input
    - Added creation of the ./osm/pbf directory.
    - Updated permissions for the new PBF directory.
    - Changed the Docker command to read from an XML file and write to a PBF file.
    - Commented out the previous command that read from a PBF file and wrote to an XML file.

--- osmosis.sh
+++ osmosis.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
 mkdir -p ./osm/xml
+mkdir -p ./osm/pbf
 
 chmod -R a+rw ./osm/xml
+chmod -R a+rw ./osm/pbf
 
 # Executar Osmosis via Docker como se fosse binário
+# docker run --rm -it \
+#     -v "$(pwd)":/data \
+#     andygol/osmosis:0.49.2 \
+#     --read-pbf /data/osm/pbf/washington-latest.osm.pbf \
+#     --write-xml /data/osm/xml/washington-latest.osm.xml \
+
+
 docker run --rm -it \
     -v "$(pwd)":/data \
     andygol/osmosis:0.49.2 \
-    --read-pbf /data/osm/pbf/washington-latest.osm.pbf \
-    --write-xml /data/osm/xml/washington-latest.osm.xml \
+    --read-xml file="/data/osm/xml/newson_krumm_reconstructed.osm.xml" \
+    --write-pbf file="/data/osm/pbf/newson_krumm_reconstructed.osm.pbf"
