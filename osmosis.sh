b613e9ad0b7dcb8f93d61930f933af305944d33b
José Souza <joseedsouza.dev@gmail.com>
2025-09-13 11:04:04 -0300

    Atualiza script osmosis.sh para corrigir comandos de execução do Docker

--- osmosis.sh
+++ osmosis.sh
@@ -7,15 +7,15 @@ chmod -R a+rw ./osm/xml
 chmod -R a+rw ./osm/pbf
 
 # Executar Osmosis via Docker como se fosse binário
-# docker run --rm -it \
-#     -v "$(pwd)":/data \
-#     andygol/osmosis:0.49.2 \
-#     --read-pbf /data/osm/pbf/washington-latest.osm.pbf \
-#     --write-xml /data/osm/xml/washington-latest.osm.xml \
-
-
 docker run --rm -it \
     -v "$(pwd)":/data \
     andygol/osmosis:0.49.2 \
-    --read-xml file="/data/osm/xml/newson_krumm_reconstructed_2.osm.xml" \
-    --write-pbf file="/data/osm/pbf/newson_krumm_reconstructed_2.osm.pbf"
+    --read-pbf /data/osm/pbf/illinois-250905.osm.pbf \
+    --write-xml /data/osm/xml/illinois-250905.osm.xml \
+
+
+# docker run --rm -it \
+#     -v "$(pwd)":/data \
+#     andygol/osmosis:0.49.2 \
+#     --read-xml file="/data/osm/xml/newson_krumm_reconstructed_2.osm.xml" \
+#     --write-pbf file="/data/osm/pbf/newson_krumm_reconstructed_2.osm.pbf"
