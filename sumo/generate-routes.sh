e19036660c6cd4baf66e0304b53b959998fc6366
José Souza <joseedsouza.dev@gmail.com>
2025-09-20 21:18:28 -0300

    adiciona script para gerar rotas a partir de simulações SUMO

---
+++ sumo/generate-routes.sh
@@ -0,0 +1,21 @@
+OUTPUT_PATH="./sumo/simulations/ohare-chicago"
+NETWORK_FILE="./sumo/simulations/ohare-chicago/ohare-filtered.net.xml"
+TAZ_FILE="./sumo/simulations/ohare-chicago/ohare-filtered.taz.add.xml"
+TRIPS_FILE="./sumo/simulations/ohare-chicago/ohare-filtered.trips.xml"
+
+# GET THE FILENAME
+BASENAME=$(basename "$NETWORK_FILE" .net.xml)
+
+OUTPUT_FILE_ROUTES="${OUTPUT_PATH}/${BASENAME}.rou.xml"
+
+export SUMO_HOME="/usr/share/sumo"
+
+
+python3 $SUMO_HOME/tools/assign/duaIterate.py \
+    -n $NETWORK_FILE  \
+    -t $TRIPS_FILE \
+    -D $TAZ_FILE \
+    -R 3600 \
+    --clean-alt \
+    -z \
+    -l 2
