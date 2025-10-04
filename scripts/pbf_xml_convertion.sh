f5ca79e0e8f0aa906f421887ef41d8e0b65e9763
José Souza <joseedsouza.dev@gmail.com>
2025-10-04 16:52:35 -0300

    atualiza caminhos de arquivos no script de conversão PBF/XML para usar 'ohare.osm'

--- scripts/pbf_xml_convertion.sh
+++ scripts/pbf_xml_convertion.sh
@@ -6,16 +6,18 @@ mkdir -p ./networks/pbf
 chmod -R a+rw ./networks/xml
 chmod -R a+rw ./networks/pbf
 
+XML_FILEPATH="./networks/xml/ohare.osm.xml"
+PBF_FILEPATH="./networks/pbf/ohare.osm.pbf"
+
 # Executar Osmosis via Docker como se fosse binário
-docker run --rm -it \
-    -v "$(pwd)":/data \
-    andygol/osmosis:0.49.2 \
-    --read-pbf /data/networks/pbf/illinois-250905.osm.pbf \
-    --write-xml /data/networks/xml/illinois-250905.osm.xml \
-
-
 # docker run --rm -it \
 #     -v "$(pwd)":/data \
 #     andygol/osmosis:0.49.2 \
-#     --read-xml file="/data/networks/xml/newson_krumm_reconstructed_2.osm.xml" \
-#     --write-pbf file="/data/networks/pbf/newson_krumm_reconstructed_2.osm.pbf"
+#     --read-pbf /data/networks/pbf/$PBF_FILEPATH \
+#     --write-xml /data/networks/xml/$XML_FILEPATH \
+
+docker run --rm -it \
+    -v "$(pwd)":/data \
+    andygol/osmosis:0.49.2 \
+    --read-xml file="/data/$XML_FILEPATH" \
+    --write-pbf file="/data/$PBF_FILEPATH"
