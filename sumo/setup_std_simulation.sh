44a77de20fc85efbd54826fb614ee361672b626e
José Souza <joseedsouza.dev@gmail.com>
2025-10-11 22:42:53 -0300

    remove variáveis de usuário e chamada para geração de rotas otimizadas no script de configuração da simulação

--- sumo/setup_std_simulation.sh
+++ sumo/setup_std_simulation.sh
@@ -1,13 +1,5 @@
 set -e
 
-if [ -n "$SUDO_USER" ]; then
-    USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
-else
-    USER_HOME=$HOME
-fi
-
-UV_PATH=$USER_HOME/.local/bin/uv
-
 echo "Warning: This script is a convienience script for setting up a standard simulation environment. It may not be suitable for all use cases."
 echo "You need to set all files and paths correctly before running this script in the other scripts."
 echo "Please review the scripts in the sumo/ directory before running this script."
@@ -22,8 +14,5 @@ sudo sh $(pwd)/sumo/generate_grid_taz.sh
 echo "--------------------------------"
 echo "Generating random trips..."
 sudo sh $(pwd)/sumo/generate_random_trips.sh
-echo "--------------------------------"
-echo "Generating optimized routes iteratively..."
-$UV_PATH run $(pwd)/sumo/generate_routes_iteratively.py
 
 
