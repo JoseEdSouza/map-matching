52219a1be5a61f681e38855af8271e16b55f617f
José Souza <joseedsouza.dev@gmail.com>
2025-10-12 13:43:46 -0300

    adiciona mapeamento de IDs de junções para IDs OSM e otimiza a conversão de IDs de veículos

--- sumo/run_simulation_save_to_parquet.py
+++ sumo/run_simulation_save_to_parquet.py
@@ -1,28 +1,82 @@
+from functools import partial
 from pathlib import Path
 from typing import cast
+
+from xml.etree import ElementTree as ET
 
 import traci
 import numpy as np
 import polars as pl
+
 from pyproj import Geod
 
-BASE_PATH = Path(__file__).parent.absolute() / "sumo/simulations/ohare-chicago"
+ROOT_PATH = Path(".").resolve().absolute()
+BASE_PATH = ROOT_PATH / "sumo/simulations/ohare-chicago"
+NETWORK_PATH = BASE_PATH / "network.net.xml"
 SIMULATION_PATH = BASE_PATH / "simulation.sumocfg"
 OUTPUT_PATH = BASE_PATH / "output"
 NOISE_METERS_STD: float | None = 5
 RANDOM_SEED = 42
 
 
+def map_junctions_to_osm_ids(network_path: Path) -> dict[str, list[str]]:
+    tree = ET.parse(network_path)
+    root = tree.getroot()
+
+    jid_to_osmid: dict[str, list[str]] = {}
+    for junction in root.iterfind("junction"):
+        jid = junction.get("id")
+        if not jid:
+            continue
+
+        for param in junction.iterfind("param"):
+            if param.get("key") != "origEdgeIds":
+                continue
+
+            edge_ids = param.get("value")
+            if not edge_ids:
+                continue
+
+            edge_ids = edge_ids.split()
+            jid_to_osmid.setdefault(jid, []).extend(edge_ids)
+
+    return jid_to_osmid
+
+
+def sumo_eid_to_osmid(jid_to_osmid: dict[str, list[str]], sumo_eid: str) -> str:
+    # :cluster_1234_0987_5678_3 -> cluster_1234_0987_5678 3
+    if "cluster" not in sumo_eid:
+        return sumo_eid
+    sumo_eid = sumo_eid.strip().strip(":")
+    parts = sumo_eid.rsplit("_", 1)
+    if len(parts) != 2:
+        return sumo_eid  # Retorna o ID original se o formato for inesperado
+    base_id, lane_index = parts
+    lane_index = int(lane_index) - 1  # Converter para índice baseado em 0
+    osmid_list = jid_to_osmid.get(base_id)
+    if not osmid_list:
+        return sumo_eid
+    if lane_index < 0 or lane_index >= len(osmid_list):
+        return sumo_eid
+    return osmid_list[lane_index]
+
+
 def main():
     cmd = ["sumo", "-c", str(SIMULATION_PATH)]
+
+    jid_to_osmid = map_junctions_to_osm_ids(NETWORK_PATH)
+    eid_to_osmid = partial(sumo_eid_to_osmid, jid_to_osmid)
 
     vehicle_ids = pl.Series(dtype=pl.Categorical)
     geo_positions = pl.Series(dtype=pl.Array(pl.Float64, shape=2))
     times = pl.Series(dtype=pl.Float64)
     edges = pl.Series(dtype=pl.Categorical)
 
+    count = 0
     traci.start(cmd)
-    while cast(int, traci.simulation.getMinExpectedNumber()) > 0:
+    while cast(int, traci.simulation.getMinExpectedNumber()) > 0 and count < 1000:
+        count += 1
+
         traci.simulation.step()
 
         current_vehicles = pl.Series(traci.vehicle.getIDList(), dtype=pl.Categorical)
@@ -41,6 +95,10 @@ def main():
 
         current_edges = current_vehicles.map_elements(
             traci.vehicle.getRoadID, return_dtype=pl.String
+        )
+
+        current_edges = current_edges.map_elements(
+            eid_to_osmid, return_dtype=pl.String
         ).cast(pl.Categorical)
 
         current_time = pl.Series(
@@ -68,7 +126,6 @@ def main():
         .cast(pl.String)
         .str.extract(r"^\D*(\d+).*$", 1)
         .alias("edge_id")
-        .cast(pl.Categorical),
     )
 
     lf = lf.with_columns(
