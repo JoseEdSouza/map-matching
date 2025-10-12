dc4756dbf9a98413aa53efb664be86ce7c1c065f
José Souza <joseedsouza.dev@gmail.com>
2025-10-11 22:43:55 -0300

    atualiza caminhos de simulação para o modelo junctionless nos scripts de lançamento do SUMO

--- sumo/launch_sumo_netedit.sh
+++ sumo/launch_sumo_netedit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NETWORK_FILE="$(pwd)/sumo/simulations/ohare-chicago-junctionless/network.net.xml"
-TAZ_FILE="$(pwd)/sumo/simulations/ohare-chicago-junctionless/grid_taz.add.xml"
+NETWORK_FILE="$(pwd)/sumo/simulations/ohare-chicago/network.net.xml"
+TAZ_FILE="$(pwd)/sumo/simulations/ohare-chicago/grid_taz.add.xml"
 
 chmod -R a+rw "$NETWORK_FILE"
 chmod -R a+rw "$TAZ_FILE"
