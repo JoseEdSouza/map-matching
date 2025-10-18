02d03951f8787ebdccb08229295d5ff3dd3e9ad7
José Souza <joseedsouza.dev@gmail.com>
2025-10-18 13:14:54 -0300

    adiciona melhorias no pipeline principal incluindo correção de trajetórias desconectadas, suporte a novos transformadores e parametização do número máximo de etapas

--- sumo/run_simulation_save_to_parquet.py
+++ sumo/run_simulation_save_to_parquet.py
@@ -14,14 +14,16 @@ import polars as pl
 
 from pyproj import Geod
 from gloe import transformer, partial_transformer
+from gloe.utils import attach, forward
 
-ROOT_PATH = Path(".").resolve().absolute()
+ROOT_PATH = Path(__file__).parent.parent.resolve()
 BASE_PATH = ROOT_PATH / "sumo/simulations/ohare-chicago-junctionless"
 ROAD_NETWORK_PATH = ROOT_PATH / "networks/graphml/ohare_network.graphml"
 SUMO_NETWORK_PATH = BASE_PATH / "network.net.xml"
 SIMULATION_PATH = BASE_PATH / "simulation.sumocfg"
+SIMULATION_MAX_STEPS: int | None = None
 OUTPUT_PATH = BASE_PATH / "output"
-NOISE_METERS_STD: float  = 5
+NOISE_METERS_STD: float = 5
 RANDOM_SEED = 42
 
 
@@ -217,6 +219,7 @@ def map_remaining_lanes_to_edges(lf: pl.LazyFrame) -> pl.LazyFrame:
 
     return lf
 
+
 @transformer
 def mark_reversed_edge_ids(lf: pl.LazyFrame) -> pl.LazyFrame:
     """Marks edge IDs that are reversed (start with '-') and removes the '-' prefix."""
@@ -227,11 +230,12 @@ def mark_reversed_edge_ids(lf: pl.LazyFrame) -> pl.LazyFrame:
 
     return lf
 
+
 @partial_transformer
 def label_unmapped_edges_as_nodes(
     lf: pl.LazyFrame, G_road: nx.MultiDiGraph
 ) -> pl.LazyFrame:
-    """ 
+    """
     Labels edge IDs that are actually node IDs by prefixing them with 'node_'.
     After this transformation, all edge IDs that correspond to nodes in the road graph
     will be clearly identified.
@@ -250,7 +254,7 @@ def label_unmapped_edges_as_nodes(
 
 @transformer
 def convert_strings_to_categorical(lf: pl.LazyFrame) -> pl.LazyFrame:
-    """ 
+    """
     Converts string columns to categorical data types for efficiency.
     Specifically, converts 'raw_lane_id', 'mapped_lane_id', and 'edge_id' to categorical types.
     """
@@ -290,7 +294,7 @@ def fill_edge_ids_backward(lf: pl.LazyFrame) -> pl.LazyFrame:
 
 @transformer
 def extract_coordinates_from_geo(lf: pl.LazyFrame) -> pl.LazyFrame:
-    """ Extracts longitude and latitude from the 'geo_position' array column."""
+    """Extracts longitude and latitude from the 'geo_position' array column."""
     lf = lf.with_columns(
         pl.col("geo_position").arr.get(0).alias("lon"),
         pl.col("geo_position").arr.get(1).alias("lat"),
@@ -301,8 +305,309 @@ def extract_coordinates_from_geo(lf: pl.LazyFrame) -> pl.LazyFrame:
     return lf
 
 
+@lru_cache(maxsize=None)
+def find_pathway(G_road: nx.MultiDiGraph, source: int, target: int) -> list[int] | None:
+    try:
+        path = nx.shortest_path(G_road, source=source, target=target, weight="length")
+        edges_path = list((x, y, 0) for x, y in zip(path[:-1], path[1:]))
+        edge_data = list(G_road.edges[edge]["osmid"] for edge in edges_path)
+
+        res = []
+        for ed in edge_data:
+            if ed not in res:
+                res.append(ed)
+        return res
+
+    except nx.NetworkXNoPath:
+        return None
+
+
+@partial_transformer
+def apply_noise(df: pl.DataFrame, noise_std: float | None = None) -> pl.DataFrame:
+    """Applies Gaussian noise to the latitude and longitude coordinates in the DataFrame."""
+    if noise_std is None:
+        return df
+
+    geod = Geod(ellps="WGS84")
+    rng = np.random.default_rng(RANDOM_SEED)
+
+    lon = df["lon"].to_numpy()
+    lat = df["lat"].to_numpy()
+
+    noise = rng.normal(0, noise_std, size=(2, len(df)))
+    noise_east = noise[0]
+    noise_north = noise[1]
+
+    azimuth_east = np.full(len(df), 90)
+    azimuth_north = np.full(len(df), 0)
+
+    lon_temp, lat_temp, _ = geod.fwd(lon, lat, azimuth_east, noise_east)
+    lon_noisy, lat_noisy, _ = geod.fwd(lon_temp, lat_temp, azimuth_north, noise_north)
+
+    geo_positions_noisy = np.column_stack((lon, lat))
+
+    noise_df = df.with_columns(
+        pl.Series(
+            geo_positions_noisy,
+            dtype=pl.Array(pl.Float64, shape=2),
+        ).alias("geo_position"),
+        pl.Series(lat_noisy, dtype=pl.Float64).alias("lat"),
+        pl.Series(lon_noisy, dtype=pl.Float64).alias("lon"),
+    )
+
+    return noise_df
+
+
 @transformer
-def run_simulation() -> pl.LazyFrame:
+def select_and_prepare_node_passages(trajectories_lf: pl.LazyFrame) -> pl.LazyFrame:
+    """
+    Filters for trajectories passing through a node and cleans required columns.
+    It prepares the data by casting and creating 'next_edge_id' and 'node_osmid'.
+    """
+    node_filtered_lf = trajectories_lf.filter(
+        pl.col("node_mapped_id").cat.starts_with("node_")
+    )
+
+    cleaned_lf = node_filtered_lf.with_columns(
+        pl.col("edge_id")
+        .cast(pl.String)
+        .cast(pl.Int64, strict=False)
+        .alias("next_edge_id"),
+        pl.col("node_mapped_id")
+        .cast(pl.String)
+        .str.replace("node_", "")
+        .cast(pl.Int64, strict=False)
+        .alias("node_osmid"),
+    ).drop_nulls(["next_edge_id", "node_osmid"])
+
+    return cleaned_lf
+
+
+@partial_transformer
+def identify_disconnected_pairs(
+    cleaned_node_lf: pl.LazyFrame,
+    G_road: nx.MultiDiGraph,
+) -> pl.LazyFrame:
+    """
+    Identifies unique (next_edge, current_node) pairs that are not directly connected in the graph.
+    """
+    osm_edges_lf = build_osm_edges_lazyframe(G_road)
+    paired_filtered_trajectories = cleaned_node_lf.with_columns(
+        pl.concat_list(
+            pl.col("next_edge_id"),
+            pl.col("node_osmid"),
+        ).alias("pairs")
+    )
+
+    unique_pairs = paired_filtered_trajectories.select("pairs").unique()
+
+    unique_pairs = unique_pairs.with_columns(
+        pl.col("pairs").list.get(0).alias("next_edge_id"),
+    )
+
+    pairs_w_edges = unique_pairs.join(
+        osm_edges_lf, left_on="next_edge_id", right_on="osmid", how="left"
+    )
+
+    pairs_w_edges = pairs_w_edges.with_columns(
+        pl.col("pairs").list.get(0).alias("next_edge_id"),
+        pl.col("pairs").list.get(1).alias("node_osmid"),
+    )
+
+    # Explodes the dataframe so each node of an edge gets its own row
+    exploded_pairs = pairs_w_edges.explode("edges")
+    exploded_pairs = exploded_pairs.with_columns(
+        pl.col("edges")
+        .list.contains(pl.col("node_osmid"))
+        .alias("connected")
+        # it means one of the nodes of the edge is the node_osmid
+    )
+
+    # Groups back by pair and checks if *any* of the edge's nodes matched
+    # aggregate to get whether there is any connection with the node_osmid and one of the edges
+    pairs_w_connectivity_status = exploded_pairs.group_by("pairs").agg(
+        pl.col("connected").any().alias("has_connection")
+    )
+
+    # join back then filter out disconnected pairs
+    disconnected_pairs_lf = (
+        pairs_w_edges.join(pairs_w_connectivity_status, on="pairs", how="left")
+        .filter(pl.col("has_connection").not_())
+        .drop("has_connection")
+    )
+
+    return disconnected_pairs_lf
+
+
+@partial_transformer
+def find_connection_pathways(
+    disconnected_pairs_lf: pl.LazyFrame, G: nx.MultiDiGraph
+) -> pl.LazyFrame:
+    """
+    For each disconnected pair, finds a valid connection pathway in the graph G.
+    """
+    pairs_w_neighbors_lf = disconnected_pairs_lf.with_columns(
+        pl.col("node_osmid")
+        .map_elements(
+            lambda node: list(G.neighbors(node)), return_dtype=pl.List(pl.Int64)
+        )
+        .alias("node_neighbors")
+    )
+
+    pairs_w_target_node_lf = pairs_w_neighbors_lf.with_columns(
+        pl.struct(["edges", "node_neighbors"])
+        .map_elements(
+            lambda x: next(
+                (u for u, v in x["edges"] if u in x["node_neighbors"]),
+                x["edges"][0][0],  # Fallback
+            ),
+            return_dtype=pl.Int64,
+        )
+        .alias("node_to_connect")
+    )
+
+    pairs_w_pathway_lf = pairs_w_target_node_lf.with_columns(
+        pl.struct(["node_osmid", "node_to_connect"])
+        .map_elements(
+            lambda x: find_pathway(G, x["node_osmid"], x["node_to_connect"]),
+            return_dtype=pl.List(pl.Int64),
+        )
+        .alias("connection_pathway")
+    ).filter(
+        (pl.col("connection_pathway").is_not_null())
+        & (pl.col("connection_pathway").list.len() > 0)
+    )
+
+    return pairs_w_pathway_lf.select("next_edge_id", "node_osmid", "connection_pathway")
+
+
+@transformer
+def generate_path_correction_rows(
+    pathways_lf: pl.LazyFrame, cleaned_node_lf: pl.LazyFrame
+) -> pl.LazyFrame:
+    """
+    Generates new trajectory rows based on the calculated connection pathways.
+    """
+    # cleaned_node_lf, pathways_lf = input_lfs
+
+    row_identifier = ["vehicle_id", "time", "next_edge_id", "node_osmid"]
+
+    disconnected_trajectories_w_pathway = cleaned_node_lf.join(
+        pathways_lf,
+        on=["next_edge_id", "node_osmid"],
+        how="inner",
+    )
+
+    print(
+        "disconnected_trajectories_w_pathway shape",
+        disconnected_trajectories_w_pathway.collect().shape,
+    )
+
+    newly_created_rows_lf = (
+        disconnected_trajectories_w_pathway
+        # 1. Explode the list of edges into separate rows
+        .explode("connection_pathway")
+        .with_columns(
+            # 2. Create an index (0, 1, 2...) for each step in the path
+            pl.cum_count("connection_pathway")
+            .over(row_identifier)
+            .alias("path_step_index")
+        )
+        .with_columns(
+            # 3. Use the index to create the incremental timestamp
+            (pl.col("time") + (pl.col("path_step_index") * 0.01)).alias("new_time"),
+            # 4. Rename 'connection_pathway' to 'edge_id' to match the original schema
+            pl.col("connection_pathway").alias("new_edge_id"),
+        )
+        # 5. Select and rename columns to build the final, clean DataFrame of new rows
+        .select(
+            pl.col("vehicle_id"),
+            pl.col("new_time").alias("time"),
+            pl.col("new_edge_id").alias("edge_id"),  # This is the corrected edge
+            pl.col("geo_position"),
+            pl.col("raw_lane_id"),
+            pl.col("node_mapped_id"),
+            pl.col("mapped_lane_id"),
+            pl.col("lat"),
+            pl.col("lon"),
+            pl.col("reversed"),
+        )
+    )
+
+    return newly_created_rows_lf
+
+
+@transformer
+def combine_and_finalize_trajectories(
+    original_lf: pl.LazyFrame,
+    new_rows_lf: pl.LazyFrame,
+) -> pl.LazyFrame:
+    """
+    Combines the original 'good' trajectories with the newly generated correction rows.
+    """
+    # original_lf, new_rows_lf = input_lfs
+
+    # Cast edge_id in original_lf to join properly
+    original_lf = original_lf.with_columns(
+        pl.col("edge_id").cast(pl.String).cast(pl.Int64, strict=False)
+    )
+
+    # Use an anti-join to get all original rows that didn't need correction
+    good_rows_lf = original_lf.join(
+        new_rows_lf, on=["vehicle_id", "node_mapped_id", "raw_lane_id"], how="anti"
+    )
+    print("anti-joined good_rows_lf shape", good_rows_lf.collect().shape)
+
+    # Align schemas before concatenation
+    new_rows_lf = new_rows_lf.select(good_rows_lf.collect_schema().names())
+
+    # Combine good rows with the new path rows
+    corrected_trajectories_lf = pl.concat([good_rows_lf, new_rows_lf])
+
+    # Sort to ensure trajectory integrity and collect the final result
+    final_lf = corrected_trajectories_lf.sort("vehicle_id", "time").with_columns(
+        pl.col("edge_id").cast(pl.String).cast(pl.Categorical)
+    )
+
+    return final_lf
+
+
+@transformer
+def collect_lazyframe(lf: pl.LazyFrame) -> pl.DataFrame:
+    return lf.collect()
+
+
+@partial_transformer
+def write_parquet(df: pl.DataFrame, path: Path) -> pl.DataFrame:
+    df.write_parquet(
+        path,
+        mkdir=True,
+        compression="zstd",
+        compression_level=3,
+        row_group_size=100_000,
+        statistics=True,
+    )
+
+    return df
+
+
+def build_osm_edges_lazyframe(G_road: nx.MultiDiGraph) -> pl.LazyFrame:
+    edges_gdf = ox.graph_to_gdfs(G_road, nodes=False, fill_edge_geometry=True).to_crs(
+        epsg=4326
+    )
+    edges_by_osmid = edges_gdf.reset_index().set_index("osmid")
+    edges_by_osmid = edges_by_osmid[["u", "v"]]
+
+    edges_pl = pl.from_pandas(edges_by_osmid, include_index=True)
+    edges_pl = edges_pl.group_by("osmid").agg(
+        pl.concat_list(pl.col("u"), pl.col("v")).alias("edges")
+    )
+    edges_pl_lazy = edges_pl.lazy()
+    return edges_pl_lazy
+
+
+@partial_transformer
+def run_simulation(_, max_steps: int | None = None) -> pl.LazyFrame:
     """Runs the SUMO simulation and collects vehicle data for each step and gathers it into a Polars DataFrame."""
     cmd = ["sumo", "-c", str(SIMULATION_PATH)]
 
@@ -312,7 +617,12 @@ def run_simulation() -> pl.LazyFrame:
     lanes = []
 
     with traci_session(cmd) as session:
+        step_count = 0
         while cast(int, session.simulation.getMinExpectedNumber()) > 0:
+            if max_steps is not None and step_count >= max_steps:
+                break
+            step_count += 1
+
             session.simulation.step()
 
             current_vehicles = np.array(session.vehicle.getIDList())
@@ -339,7 +649,7 @@ def run_simulation() -> pl.LazyFrame:
             times.append(current_time)
             lanes.append(current_lanes)
 
-    df = pl.DataFrame(
+    lf = pl.LazyFrame(
         (
             pl.Series(np.concatenate(vehicle_ids)).cast(pl.Int64).alias("vehicle_id"),
             pl.Series(np.concatenate(geo_positions))
@@ -350,90 +660,94 @@ def run_simulation() -> pl.LazyFrame:
         )
     )
 
-    return df.lazy()
+    return lf
 
 
 @partial_transformer
-def apply_noise(
-    df: pl.DataFrame, noise_std_meters: float | None = None
-) -> pl.DataFrame:
-    """Applies Gaussian noise to the latitude and longitude coordinates in the DataFrame."""
-    if noise_std_meters is None:
-        return df
-
-    geod = Geod(ellps="WGS84")
-    rng = np.random.default_rng(RANDOM_SEED)
-
-    lon = df["lon"].to_numpy()
-    lat = df["lat"].to_numpy()
-
-    noise = rng.normal(0, noise_std_meters, size=(2, len(df)))
-    noise_east = noise[0]
-    noise_north = noise[1]
-
-    azimuth_east = np.full(len(df), 90)
-    azimuth_north = np.full(len(df), 0)
-
-    lon_temp, lat_temp, _ = geod.fwd(lon, lat, azimuth_east, noise_east)
-    lon_noisy, lat_noisy, _ = geod.fwd(lon_temp, lat_temp, azimuth_north, noise_north)
-
-    geo_positions_noisy = np.column_stack((lon, lat))
-
-    noise_df = df.with_columns(
-        pl.Series(
-            geo_positions_noisy,
-            dtype=pl.Array(pl.Float64, shape=2),
-        ).alias("geo_position"),
-        pl.Series(lat_noisy, dtype=pl.Float64).alias("lat"),
-        pl.Series(lon_noisy, dtype=pl.Float64).alias("lon"),
-    )
-
-    return noise_df
-
-
-@transformer
-def collect_lazyframe(lf: pl.LazyFrame) -> pl.DataFrame:
-    return lf.collect()
-
-@partial_transformer
-def write_parquet(df: pl.DataFrame, path: Path) -> pl.DataFrame:
-    df.write_parquet(
-        path,
-        mkdir=True,
-        compression="zstd",
-        compression_level=3,
-        row_group_size=100_000,
-        statistics=True,
-    )
-
-    return df
+def print_shape(lf: pl.LazyFrame, context: str) -> pl.LazyFrame:
+    print("Shape after", context, ":", lf.collect().shape)
+    return lf
 
 
 def main() -> None:
     net = sumolib.net.readNet(str(SUMO_NETWORK_PATH))
     G_road = ox.load_graphml(ROAD_NETWORK_PATH)
 
-    pipeline = (
-        run_simulation
-        >> map_internal_junctions_to_edges(G_road, net)
+    extract_lane_to_osmid = (
+        map_internal_junctions_to_edges(G_road, net)
         >> map_remaining_lanes_to_edges
         >> mark_reversed_edge_ids
         >> label_unmapped_edges_as_nodes(G_road)
         >> convert_strings_to_categorical
         >> fill_edge_ids_backward
         >> extract_coordinates_from_geo
-        >> collect_lazyframe >> (
-            write_parquet(OUTPUT_PATH / "fcd_raw.parquet"),
-            apply_noise(NOISE_METERS_STD) >> write_parquet(OUTPUT_PATH / "fcd_noisy_raw.parquet")
+    )
+
+    @transformer
+    def custom_debug(in_data):
+        return in_data
+
+    apply_pathway_correction = (
+        forward[pl.LazyFrame]()
+        >> print_shape("initial_trajectories")
+        >> (
+            forward[pl.LazyFrame](),
+            select_and_prepare_node_passages
+            >> print_shape("selected_node_passages")
+            >> custom_debug
+            >> attach(
+                identify_disconnected_pairs(G_road)
+                >> print_shape("identified_disconnected_pairs")
+                >> custom_debug
+                >> find_connection_pathways(G_road)
+                >> print_shape("found_pathways")
+                >> custom_debug
+            )
+            >> generate_path_correction_rows
+            >> print_shape("generated_correction_rows")
+            >> custom_debug,
+        )
+        >> combine_and_finalize_trajectories
+        >> print_shape("finalized_trajectories")
+        >> custom_debug
+    )
+
+    @partial_transformer
+    def load_fcd_data(_, path) -> pl.LazyFrame:
+        return pl.scan_parquet(path)
+
+    # pipeline_2 = (
+    #     load_fcd_data(path=OUTPUT_PATH / "fcd.parquet")
+    #     >> apply_pathway_correction
+    #     >> collect_lazyframe
+    # )
+    #
+    # df = pipeline_2()
+    # print(df)
+
+    pipeline = (
+        run_simulation(max_steps=SIMULATION_MAX_STEPS)
+        >> extract_lane_to_osmid
+        >> (
+            collect_lazyframe >> write_parquet(OUTPUT_PATH / "fcd_raw.parquet"),
+            collect_lazyframe
+            >> apply_noise(noise_std=NOISE_METERS_STD)
+            >> write_parquet(OUTPUT_PATH / "fcd_noisy.parquet"),
+            apply_pathway_correction
+            >> collect_lazyframe
+            >> write_parquet(OUTPUT_PATH / "fcd_resolved.parquet"),
         )
     )
 
-    df, df_noisy = pipeline()
+    df, df_noisy, df_resolved = pipeline()
     print("Raw simulation data saved to", OUTPUT_PATH / "fcd_raw.parquet")
     print(df)
 
     print("Noisy raw simulation data saved to", OUTPUT_PATH / "fcd_noisy_raw.parquet")
     print(df_noisy)
+
+    print("Resolved pathway data saved to", OUTPUT_PATH / "fcd_resolved.parquet")
+    print(df_resolved)
 
 
 if __name__ == "__main__":
